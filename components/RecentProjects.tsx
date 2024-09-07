b7b8a7326c5d2b6f927bbfc72da8775e5d1d40e3
AdarshSinha7 <adarshsinha.gkp@gmail.com>
2024-09-08 00:18:31 +0530

    sentry added

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { projects } from '@/data'
 import React from 'react'
 import { PinContainer } from './ui/3d-pin'
@@ -6,7 +8,7 @@ import { FaLocationArrow } from 'react-icons/fa'
 
 const RecentProjects = () => {
   return (
-    <div className='py-20'>
+    <div className='py-20' id="projects">
         <h1 className='heading'>
             A small section of {' '}
             <span className='text-purple'>Recent Projects</span>
@@ -14,7 +16,7 @@ const RecentProjects = () => {
         <div className='flex flex-wrap items-center justify-center p-4 gap-16 mt-10'>
             {projects.map(({id, title, des, img, iconLists, link})=>(
                 <div key={id} className='lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]'>
-                    <PinContainer title={title} href={link}> 
+                    <PinContainer title={link} href={link}> 
                         <div className='relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10'>
                             <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
                                 <img src='/bg.png' alt='bg-img'/>
@@ -38,8 +40,8 @@ const RecentProjects = () => {
                                 </div>
                                 ))}
                             </div>
-                            <div >
-                                <p>Check Live Site</p>
+                            <div className='flex justify-center items-center'>
+                                <p className='flex lg:text-xl md:text-xs text-sm text-purple'>Check Live Site</p>
                                 <FaLocationArrow className='ms-3' color='#CBACF9'/>
                             </div>
                         </div>
